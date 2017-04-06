593d29d25a88a0d2c4df73c1f6651e01588177bc
Konrad Grzanek <kongra@gmail.com>
2017-04-06 16:51:45 +0200

    Added ToInputStream protocol and some impls.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kongra/prelude "0.1.4"
+(defproject kongra/prelude "0.1.5-SNAPSHOT"
   :description      "Predule codebase for Clojure"
   :url              "https://github.com/kongra/prelude"
   :license          {:name   "Eclipse Public License"
