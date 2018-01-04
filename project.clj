d5a3cd6b534d464a8f1ded9e6af18b02d091d1d1
kongra <kongra@gmail.com>
2018-01-04 20:32:57 +0100

    Libraries update

--- project.clj
+++ project.clj
@@ -1,23 +1,24 @@
-(defproject kongra/prelude "0.1.7"
+(defproject kongra/prelude "0.1.8"
   :description      "Predule codebase for Clojure"
   :url              "https://github.com/kongra/prelude"
   :license          {:name   "Eclipse Public License"
                      :url    "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies     [[org.clojure/clojure                  "1.8.0"]
+  :dependencies     [[org.clojure/clojure                  "1.9.0"]
                      [primitive-math                       "0.1.6"]
                      [org.clojure/math.numeric-tower       "0.0.4"]
-                     [org.apache.commons/commons-lang3       "3.6"]
+                     [org.apache.commons/commons-lang3       "3.7"]
                      [org.uncommons.maths/uncommons-maths "1.2.2a"]
-                     [kongra/ch                            "0.1.7"]]
+                     [kongra/ch                            "0.1.8"]]
 
-  :profiles {:repl  {:plugins [[lein-nodisassemble "0.1.3"]]}}
-
+  :profiles {:repl  {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
+                     :plugins      [[lein-nodisassemble       "0.1.3"]
+                                    [cider/cider-nrepl       "0.16.0"]]}}
   :aot               :all
 
   :source-paths      ["src/clj" "test"]
-  :java-source-paths ["src/java"]
-  :test-paths        ["test"    ]
+  :java-source-paths ["src/java"      ]
+  :test-paths        ["test"          ]
 
   :global-vars       {*warn-on-reflection* true
                       *assert*             true
