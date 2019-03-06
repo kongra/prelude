5f7a6d1d56e7d17365579c8c2815dc8aaca19f8d
Konrad Grzanek <kongra@gmail.com>
2019-03-06 12:10:00 +0100

    Added captArgs functionality (rewritten from R)

---
+++ src/main/clojure/clojure/kongra/prelude/captargs.clj
@@ -0,0 +1,27 @@
+;; Copyright (c) 2016-present Konrad Grzanek
+;; Created 2019-03-06
+(ns clojure.kongra.prelude.captargs
+  (:require
+   [clojure.kongra.ch
+    :refer [chSome chOptional chIfn]]))
+
+(set! *warn-on-reflection* true)
+
+(def ^:private CAPT-ARGS (atom {}))
+
+(defn captArgs
+  [& {:keys [id f args]}]
+  (chSome          id)
+  (chOptional chIfn f)
+
+  (if (nil? f)
+    (@CAPT-ARGS id)
+
+    (do (swap! CAPT-ARGS assoc id args)
+        (apply f args))))
+
+(defn callArgs
+  [& {:keys [id f]}]
+  (chSome id)
+  (chIfn   f)
+  (apply f (captArgs :id id)))
