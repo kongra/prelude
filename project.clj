7fa03fa073a949a0cad034445ea4c56f776c77e5
kongra <kongra@gmail.com>
2020-03-19 13:06:51 +0100

    Version bump.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright (c) Konrad Grzanek
 ;; Created 2019-01-05
-(defproject kongra/prelude "0.1.19"
+(defproject kongra/prelude "0.1.20"
   :description  "Predule codebase for Clojure"
   :url          "https://github.com/kongra/prelude"
   :license      {:name   "Eclipse Public License"
